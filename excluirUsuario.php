9909ff1ef8cc82db410ea0e9891b12ac8fec913a
Fulano de Tal <aryssonmenezes@gmail.com>
2023-07-05 14:26:46 -0300

    edição e exclusao de usuarios

---
+++ excluirUsuario.php
@@ -0,0 +1,18 @@
+<?php
+include_once "getUsuarios.php";
+
+ if(isset($_GET['id']))  {
+    $id = $_GET['id'];
+
+   $usuario = buscarUsuario($id);
+
+    if(!$usuario){
+        die("usuario inexistente");
+    }else{
+        excluirUsuario($id);
+        header('Location: todosUsuarios.php');
+    }
+
+}
+
+?>
--- getUsuarios.php
+++ getUsuarios.php
@@ -10,7 +10,48 @@ $usuarios = $conn->query($sql);
 $conn->close();
 
 return $usuarios;
+}
 
+function buscarUsuario($id){
+    $conn = conectarBanco();
+    $sql = "select * from usuarios where id = '$id' ";
+    $resultado = $conn->query($sql);
+
+    if($resultado->num_rows>0){
+        return $resultado->fetch_assoc();
+    }else {
+        return null;
+    }
+
+
+}
+function atualizarUsuario($id,$usuario,$email){
+
+    $conn =conectarBanco();
+$sql = "update usuarios set usuario = ? ,email =? where id = ?";
+$smt = $conn->prepare($sql);
+$smt->bind_param('ssi',$usuario,$email,$id);
+
+if($smt->execute()){
+    echo 'Dados Atualizados com Sucesso';
+}else{
+    echo "error ao atualizar".  mysqli_error ($conn) ;
+}
+
+
+}
+function excluirUsuario($id){
+    echo "$id";
+    $conn = conectarBanco();
+    $sql = "DELETE FROM usuarios WHERE id = ?";
+    $smt  =$conn->prepare($sql);
+    $smt->bind_param('i', $id);
+
+    if($smt->execute()){
+        echo "<script>alert('Usuario excluido'); </script>";
+    }else{
+        echo "<script>alert('erro ao excluido'); </script>";
+    }
 }
 
 ?>
